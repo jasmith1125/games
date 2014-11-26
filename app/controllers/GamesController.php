031391306b6f2c5a081c14db274705d284f2cde5
jasmith1125 <jasmith1125@yahoo.com>
2014-11-26 16:24:45 -0500

    first commit

---
+++ app/controllers/GamesController.php
@@ -0,0 +1,66 @@
+<?php
+
+// app/controllers/GamesController.php
+
+class GamesController extends BaseController
+{
+    public function index()
+    {
+        // Show a listing of games.
+        $games = Game::all();
+
+        return View::make('index', compact('games'));
+    }
+
+    public function create()
+    {
+        // Show the create game form.
+        return View::make('create');
+    }
+
+    public function handleCreate()
+    {
+        // Handle create form submission.
+        $game = new Game;
+        $game->title        = Input::get('title');
+        $game->publisher    = Input::get('publisher');
+        $game->complete     = Input::has('complete');
+        $game->save();
+
+        return Redirect::action('GamesController@index');
+    }
+
+    public function edit(Game $game)
+    {
+        // Show the edit game form.
+        return View::make('edit', compact('game'));
+    }
+
+    public function handleEdit()
+    {
+        // Handle edit form submission.
+        $game = Game::findOrFail(Input::get('id'));
+        $game->title        = Input::get('title');
+        $game->publisher    = Input::get('publisher');
+        $game->complete     = Input::has('complete');
+        $game->save();
+
+        return Redirect::action('GamesController@index');
+    }
+
+    public function delete(Game $game)
+    {
+        // Show delete confirmation page.
+        return View::make('delete', compact('game'));
+    }
+
+    public function handleDelete()
+    {
+        // Handle the delete confirmation.
+        $id = Input::get('game');
+        $game = Game::findOrFail($id);
+        $game->delete();
+
+        return Redirect::action('GamesController@index');
+    }
+}
---
+++ app/routes.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+
+Route::get('/get-environment',function() {
+
+    echo "Environment: ".App::environment();
+
+});
+
+// Bind route parameters.
+Route::model('game', 'Game');
+
+// Show pages.
+Route::get('/', 'GamesController@index');
+Route::get('/create', 'GamesController@create');
+Route::get('/edit/{game}', 'GamesController@edit');
+Route::get('/delete/{game}', 'GamesController@delete');
+
+// Handle form submissions.
+Route::post('/create', 'GamesController@handleCreate');
+Route::post('/edit', 'GamesController@handleEdit');
+Route::post('/delete', 'GamesController@handleDelete');
